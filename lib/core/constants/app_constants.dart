eb06c6a659cccb6f3ee8f49506b4e5518e221c4f
vinirod <vinicioswork@gmail.com>
2026-06-24 16:34:36 -0300

    chore(core): add constants + usecase

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,14 @@
+class AppConstants {
+  AppConstants._();
+
+  static const int pageSize = 20;
+  static const int minDescriptionLength = 10;
+}
+
+class ApiConstants {
+  ApiConstants._();
+  // (todo) Definir base url e porta
+  static const String baseUrl = '';
+  static const String login = '/login';
+  static const String requests = '/requests';
+}
